d037a315ca242f7461ce18b4698eab75e49c690f
sebamuzzu@gmail.com <sebamuzzu@gmail.com>
2025-10-04 21:54:29 -0300

    cypress.config.js update

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,9 @@
-{
-  "baseUrl":"http://whatever.com"
-}
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
